0ec71593afed86cdbe68c07703e08f1eafbc841b
Talha <mdtalha2008@gmail.com>
2022-11-11 23:12:19 +0600

    update news

--- src/components/Home.js
+++ src/components/Home.js
@@ -46,7 +46,7 @@ const Home = () => {
                     <img src="https://placeimg.com/260/400/arch" className="w-full h-96 rounded-lg shadow-2xl" alt='...' />
                     <div className='w-full text-slate-900'>
                         <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p className="py-6">Fans on social media have often been spotted endorsing the latest box office collections of the movies featuring their treasured stars, to promote them further.</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
@@ -57,7 +57,7 @@ const Home = () => {
                     <img src="https://placeimg.com/260/400/arch" className="w-full h-96 rounded-lg shadow-2xl" alt='...' />
                     <div className='w-full text-slate-900'>
                         <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p className="py-6">Box Office news, data and analysis covering the US, UK and international ... 'Black Adam' passes $300m at global box office; Japanese anime 'One Piece Film .</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
