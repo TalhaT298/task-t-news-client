c5b3c1c5f14a18e2ac186a9e157407823e7e2775
Talha <mdtalha2008@gmail.com>
2022-11-11 23:13:16 +0600

    login updtae

--- src/components/Login.js
+++ src/components/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
             <div className="hero-content flex-col lg:flex-row-reverse gap-20">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Login Now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">You can log in with your signNow, airSlate, or pdfFiller accounts. Facebook. Google. Microsoft. OR. Email is required. Password is required..</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
