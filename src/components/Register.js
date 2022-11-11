c91aaeb776a6ecea44326db30c1763c507b42fd3
Talha <mdtalha2008@gmail.com>
2022-11-11 23:14:36 +0600

    register update

--- src/components/Register.js
+++ src/components/Register.js
@@ -27,7 +27,7 @@ const Register = () => {
             <div className="hero-content flex-col lg:flex-row-reverse gap-20">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Register Now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">Inspiring your connection to sports events ⚡️Whether you're an athlete or organiser Get passionate Get organised..</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
